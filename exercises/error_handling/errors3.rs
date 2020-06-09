5c0d307909d5e6793ef4b8b0c9461483f9488b5d
Matthew Fahrenkrug <mfahrenkrug@gmail.com>
2020-06-08 23:24:04 -0400

    My solutions for rustlings.
    No guarantees on if they're idiomatic, but they pass the spirit and
    the letter of the tests.

--- exercises/error_handling/errors3.rs
+++ exercises/error_handling/errors3.rs
@@ -4,7 +4,6 @@
 // Why not? What should we do to fix it?
 // Execute `rustlings hint errors3` for hints!
 
-// I AM NOT DONE
 
 use std::num::ParseIntError;
 
@@ -12,7 +11,12 @@ fn main() {
     let mut tokens = 100;
     let pretend_user_input = "8";
 
-    let cost = total_cost(pretend_user_input)?;
+    let cost = total_cost(pretend_user_input);
+
+    let cost = match cost {
+        Ok(num) => num,
+        Err(error) => panic!("Problem parsing int: {:?}", error),
+    };
 
     if cost > tokens {
         println!("You can't afford that many!");
